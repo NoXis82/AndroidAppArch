9c5b6b2af401ababadbaa2aac9c20419ff9d228e
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2025-10-16 22:50:43 +0300

    setup common navigation

---
+++ app/src/main/java/ru/noxis/common/navigation/Direction.kt
@@ -0,0 +1,14 @@
+package ru.noxis.common.navigation
+
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavDeepLink
+import androidx.navigation.NavOptionsBuilder
+
+abstract class Direction(protected val target: String) {
+    open val route: String = target
+    open val arguments: List<NamedNavArgument> get() = emptyList()
+    open val deepLinks: List<NavDeepLink> get() = emptyList()
+
+    protected fun createNavAction(destination: String = target, builder: (NavOptionsBuilder.() -> Unit)? = null) =
+        NavigationAction(destination = destination, builder = builder)
+}
---
+++ app/src/main/java/ru/noxis/common/navigation/NavigationAction.kt
@@ -0,0 +1,15 @@
+package ru.noxis.common.navigation
+
+import androidx.navigation.NavOptions
+import androidx.navigation.NavOptionsBuilder
+import androidx.navigation.navOptions
+
+class NavigationAction private constructor(
+    val destination: String,
+    val navOptions: NavOptions? = null,
+) {
+    constructor(
+        destination: String,
+        builder: (NavOptionsBuilder.() -> Unit)? = null,
+    ) : this(destination, builder?.let(::navOptions))
+}
